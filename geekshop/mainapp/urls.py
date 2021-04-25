b617cfc1b2b44a8c7042ef8c9c4317f03995485b
matos85 <matos85@mail.ru>
2021-04-25 17:41:41 +0300

    lesson1

---
+++ geekshop/mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import main, products, contact
+
+urlpatterns = [
+    path('', main),
+    path('products/', products),
+    path('contact/', contact),
+]
