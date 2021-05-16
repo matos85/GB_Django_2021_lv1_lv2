dc9a9ce3ee9cefe5f6eecc5265c76b6b2d7ddef1
matos85 <matos85@mail.ru>
2021-05-16 11:39:18 +0300

    залил базу

---
+++ mainapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from mainapp.models import ProductCategory, Product
+
+
+def products(request, pk=None):
+    print(pk)
+    title = 'продукты'
+
+    categories = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category_id__pk=pk).order_by('price')
+
+    context = {
+        'title': title,
+        'categories': categories,
+        'category': category,
+        'products': products,
+    }
+    return render(request, 'products_list.html', context=context)
+
