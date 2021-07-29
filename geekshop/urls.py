ac0b0e10fa72c48dde7b2bf382dfa9d0b2b6c541
matos85 <matos85@mail.ru>
2021-07-29 19:37:40 +0300

    lesson14_new2

---
+++ geekshop/urls.py
@@ -0,0 +1,44 @@
+"""geekshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from mainapp import urls
+from .views import contacts, main
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+app_name = 'geekshop'
+
+urlpatterns = [
+    path('', main, name='index'),
+    path('admin/', admin.site.urls),
+    path('contacts/', contacts, name='contacts'),
+    path('products/', include(urls, namespace='products'),),
+    path('auth/', include('authapp.urls', namespace='auth')),
+    path('basket/', include('basketapp.urls', namespace='basket')),
+    path('', include('social_django.urls', namespace='social')),
+    path('admin_staff/', include('adminapp.urls', namespace='admin_staff')),
+    path('order/', include('ordersapp.urls', namespace='order')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+   import debug_toolbar
+
+   urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
